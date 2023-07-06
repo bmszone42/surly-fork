194cd9120a30feb1bb4dbff283b2d0d4aebb422d
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-07-06 17:22:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,32 +86,27 @@ if run_simulation:
     # Perform the simulations
     df = simulate(num_simulations, overhead_range, cots_chips_range, custom_chips_range, custom_chips_nre_range, custom_chips_licensing_range, ebrick_chiplets_range, ebrick_chiplets_licensing_range, osat_range, vv_tests_range, profit_margin_range)
 
-    # Data Summary
-    st.subheader('Data Summary')
-    cost_summary = df['Total Cost'].describe()
-    st.write(cost_summary)
+    # Plot the histogram of total costs
+    st.subheader('Histogram of Total Costs')
+    fig = px.histogram(df, x='Total Cost', nbins=num_bins, marginal='box')
+    st.plotly_chart(fig)
 
-    # Box Plot of Total Cost
-    st.subheader('Box Plot of Total Cost')
-    fig_boxplot = px.box(df, y='Total Cost')
-    st.plotly_chart(fig_boxplot)
+    # Identify the largest cost drivers
+    st.subheader('Largest Cost Drivers')
+    cost_drivers = df.drop(columns='Total Cost').mean().sort_values(ascending=False)
+    st.write(cost_drivers)
 
-    # Correlation Heatmap
-    st.subheader('Correlation Heatmap')
-    correlation_matrix = df.drop(columns=['Profit', 'Total Cost']).corr()
-    fig_heatmap = px.imshow(correlation_matrix)
-    st.plotly_chart(fig_heatmap)
+    # Identify the ideal value range for each variable to bring the average total cost below $5M
+    st.subheader('Ideal Value Range for Each Variable')
+    for column in df.columns:
+        if column != 'Total Cost':
+            ideal_range = df[df['Total Cost'] < 5e6][column].agg(['min', 'max'])
+            st.write(f'{column}: {ideal_range[0]} - {ideal_range[1]}')
 
-    # Pairwise Scatter Plot
-    st.subheader('Pairwise Scatter Plot')
-    fig_scatter = px.scatter_matrix(df, dimensions=df.columns[:-2])
-    st.plotly_chart(fig_scatter)
-
-    # Data Filtering
-    st.subheader('Data Filtering')
-    profit_margin_filter = st.slider('Filter by Profit Margin', min_value=0.0, max_value=1.0, value=0.5, step=0.01)
-    filtered_df = df[df['Profit'] / df['Total Cost'] >= profit_margin_filter]
-    st.write(filtered_df)
+    # Identify the profit margin needed to keep the average total cost below $5M
+    st.subheader('Profit Margin Needed')
+    profit_margin_needed = round(df[df['Total Cost'] < 5e6]['Profit'].mean() / df[df['Total Cost'] < 5e6].drop(columns='Profit').sum(axis=1).mean(), 2)
+    st.write(f'Profit margin needed to keep the average total cost below $5M: {profit_margin_needed * 100}%')
 
 # Downloadable results
 if st.button('Download Results as CSV'):
